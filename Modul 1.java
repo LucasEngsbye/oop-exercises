dcc5bce61c1a13034b2a49d7f099cfb587e29b16
LucasEngsbye <lucasengsbye@gmail.com>
2023-10-04 12:26:41 +0200

    Moving

---
+++ Modul 1.java
@@ -0,0 +1,7 @@
+Hello, World
+
+PowerShell commands
+cd - change directory, for at komme frem til den mappe hvor filen er i, eksempel "cd C:\Users\lucas\Documents\SublimeText\1Semester" for at komme hen til den. "cd .." for at gå en mappe tilbage
+ls - liste over filer der er i mappen, viser også størrelse og andet
+mkdir "*name*" - opretter en mappe i den mappe du er i
+mv - bruges til at flytte filer bl.a. som eksempel mv "*file name*" "*folder name*"
